8b903e5ee15c53d1012531330645628e6f54c51b
the-anisa <theabfanisa@gmail.com>
2025-03-19 01:55:15 +0600

    added window panel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,7 +6,10 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeight = 650;
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac_Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
     TicTacToe(){
         //frame window done 
         frame.setVisible(true);
@@ -14,7 +17,23 @@ public class TicTacToe {
         //to set frame in the center
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout()); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //for exiting on close
+        frame.setLayout(new BorderLayout());
+
+        //setting up the window
+        textLabel.setBackground(Color.ORANGE);
+        textLabel.setForeground(Color.DARK_GRAY);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+
+
+     }
     }
-}
